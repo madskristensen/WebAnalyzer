96acc5fae12bf3c77ebf02e343348e7d2c4b7a49
Mads Kristensen <post@madskristensen.net>
2015-10-05 10:26:00 -0700

    Fixed telemetry key

--- src/WebLinter/Telemetry.cs
+++ src/WebLinter/Telemetry.cs
@@ -9,7 +9,7 @@ namespace WebLinter
     public static class Telemetry
     {
         private static TelemetryClient _telemetry = GetAppInsightsClient();
-        private const string TELEMETRY_KEY = "6e6f3a28-9a6b-4338-a03d-560756b25a40";
+        private const string TELEMETRY_KEY = "468caa30-c256-47c7-946d-1f0230ccec9d";
 
         /// <summary>Determines if telemetry should be reported.</summary>
         public static bool Enabled { get; set; } = true;
